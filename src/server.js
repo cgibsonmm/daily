45c69f56f6462bdb3a29866b50ca379fa00c4521
corey gibson <cgibsonmm@gmail.com>
2021-03-11 14:28:56 -0500

    commeted out appollo server

--- src/server.js
+++ src/server.js
@@ -10,41 +10,41 @@ const dev = NODE_ENV === "development";
 
 const app = express();
 
-// polka() // You can also use Express
-//   .use(
-//     compression({ threshold: 0 }),
-//     sirv("static", { dev }),
-//     sapper.middleware()
-//   )
-//   .listen(PORT, (err) => {
-//     if (err) console.log("error", err);
-//   });
+polka() // You can also use Express
+  .use(
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
 
 // app.get("/", (req, res) => {
 //   console.log("HERE");
 // });
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+// const typeDefs = gql`
+//   type Query {
+//     hello: String
+//   }
+// `;
 
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!",
-  },
-};
+// // Provide resolver functions for your schema fields
+// const resolvers = {
+//   Query: {
+//     hello: () => "Hello world!",
+//   },
+// };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+// const server = new ApolloServer({ typeDefs, resolvers });
 
-server.applyMiddleware({ app, path: "graphql" });
+// server.applyMiddleware({ app, path: "graphql" });
 
-app.use(sapper.middleware({ ignore: "/graphql" }));
-server.applyMiddleware({ app });
+// app.use(sapper.middleware({ ignore: "/graphql" }));
+// server.applyMiddleware({ app });
 
-app.listen({ port: 3000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`)
-);
+// app.listen({ port: 3000 }, () =>
+//   console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`)
+// );
