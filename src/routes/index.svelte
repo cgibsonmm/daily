fcda02758ba81e4e85fb914afcbb81bdf2e6cd53
corey gibson <cgibsonmm@gmail.com>
2021-03-11 12:15:27 -0500

    working conection to electron

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,26 @@
+<script>
+	import {onMount} from 'svelte'
+	import isElectron from 'is-electron'
+
+
+	/// Used to have comms with electorn ///
+
+	// onMount(()=> {
+	// 	if(isElectron()){
+
+	// 		window.ipcRenderer.on('asynchronous-reply', (event, arg) => {
+	// 			console.log(arg) // prints "pong"
+	// 		})
+	// 		window.ipcRenderer.send('asynchronous-message', 'ping')
+	// 		window.ipcRenderer.on('pong', (event, arg) => {
+	// 			console.log(arg)
+	// 		})
+	// 		// window.ipcRenderer.send('ping')
+	// 	}
+	// })
+
+</script>
+
 <svelte:head>
 	<title>Sapper Electron App Template</title>
 </svelte:head>
